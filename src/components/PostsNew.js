00c290c1369541d8c4262d46e5fe55c39cf55091
Evelyn Sharp <yihongshi90@gmail.com>
2017-07-23 15:09:30 -0600

    callback navigation

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -35,7 +35,10 @@ class PostsNew extends React.Component {
   }
 
   onSubmit(values) {
-    this.props.createPost(values)
+    this.props.createPost(values, () => {
+      this.props.history.push('/');      
+    });
+
   }
 
   render() {
